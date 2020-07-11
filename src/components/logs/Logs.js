6649d0970048bfe142336b67f13f7b640179a1ab
Nagaraj Kulkarni <Nagaraj.Kulkarni@zapcg.com>
2020-07-11 20:12:07 +0530

    initial commit

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import LogItem from "./LogItem";
+import Preloader from "../layout/Preloader";
+
+export const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+
+    // eslint-disable-next-line
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <Preloader />;
+  }
+  return (
+    <div>
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h4 className="center">System logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className="center">No logs</p>
+        ) : (
+          logs.map((log) => <LogItem log={log} key={log.id}></LogItem>)
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Logs;
